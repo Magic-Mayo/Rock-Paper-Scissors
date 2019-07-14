5f25e3ecc855ce30bfb0399a0f3bef10a33d4633
Magic-Mayo <mike.mayoii@gmail.com>
2019-07-14 11:51:02 -0700

    commented out auth js and link for testing

--- assets/js/firebaseauth.js
+++ assets/js/firebaseauth.js
@@ -1,19 +1,19 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyBr2YPQyeL0INt0EIYRDmqvsf0MNP1U8P0",
-    authDomain: "rock-paper-scissors-a9a8f.firebaseapp.com",
-    databaseURL: "https://rock-paper-scissors-a9a8f.firebaseio.com",
-    projectId: "rock-paper-scissors-a9a8f",
-    storageBucket: "",
-    messagingSenderId: "267978544107",
-    appId: "1:267978544107:web:6d8088efd8b9ba61"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// var firebaseConfig = {
+//     apiKey: "AIzaSyBr2YPQyeL0INt0EIYRDmqvsf0MNP1U8P0",
+//     authDomain: "rock-paper-scissors-a9a8f.firebaseapp.com",
+//     databaseURL: "https://rock-paper-scissors-a9a8f.firebaseio.com",
+//     projectId: "rock-paper-scissors-a9a8f",
+//     storageBucket: "",
+//     messagingSenderId: "267978544107",
+//     appId: "1:267978544107:web:6d8088efd8b9ba61"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+//   const auth = firebase.auth
 
-
-  firebase.auth().signInAnonymously().catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  });
+//   auth().signInAnonymously().catch(function(error) {
+//     // Handle Errors here.
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     // ...
+//   });
